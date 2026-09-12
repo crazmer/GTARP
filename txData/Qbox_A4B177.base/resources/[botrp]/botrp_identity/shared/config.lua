d1651fc5e2c19493773ef8a92cce276ea041d8b7
Atharva Mali <40512661+crazmer@users.noreply.github.com>
2026-09-12 19:30:23 +0530

    feat(botrp_identity): bump identity service to v0.2.0

--- txData/Qbox_A4B177.base/resources/[botrp]/botrp_identity/shared/config.lua
+++ txData/Qbox_A4B177.base/resources/[botrp]/botrp_identity/shared/config.lua
@@ -1,7 +1,7 @@
 BotRPIdentity = BotRPIdentity or {}
 
 BotRPIdentity.Config = {
-    version = '0.1.1',
+    version = '0.2.0',
     events = {
         ready = 'botrp:identity:server:ready',
         updated = 'botrp:identity:client:updated',
