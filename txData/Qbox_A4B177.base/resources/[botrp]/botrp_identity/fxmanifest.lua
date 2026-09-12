dec76a9d4330056341df4bc680c6b79d85b15125
Atharva Mali <40512661+crazmer@users.noreply.github.com>
2026-09-12 19:00:25 +0530

    feat: add BotRP identity resource

---
+++ txData/Qbox_A4B177.base/resources/[botrp]/botrp_identity/fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'botrp_identity'
+description 'BotRP normalized character identity service'
+version '0.1.0'
+author 'BotRP'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/config.lua',
+}
+
+client_scripts {
+    'client/main.lua',
+}
+
+server_scripts {
+    'server/main.lua',
+}
+
+dependencies {
+    'ox_lib',
+    'qbx_core',
+    'botrp_core',
+}
+
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
