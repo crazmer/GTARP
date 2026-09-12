22ccad69bab13bfd48e3b03fc5f1bf11eb1792bc
Atharva Mali <40512661+crazmer@users.noreply.github.com>
2026-09-12 19:03:36 +0530

    feat: add BotRP audit resource

---
+++ txData/Qbox_A4B177.base/resources/[botrp]/botrp_audit/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'botrp_audit'
+author 'BotRP'
+description 'BotRP centralized audit and structured logging foundation'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+lua54 'yes'
