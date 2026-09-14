384f0c1ad0275afcfb79d6da2089b46a4cee58ad
Atharva Mali <40512661+crazmer@users.noreply.github.com>
2026-09-14 23:42:40 +0530

    Activate v1.0.7 fitted translucent character HUD

--- txData/Qbox_A4B177.base/resources/[botrp]/botrp_character/fxmanifest.lua
+++ txData/Qbox_A4B177.base/resources/[botrp]/botrp_character/fxmanifest.lua
@@ -3,14 +3,14 @@ game 'gta5'
 
 name 'botrp_character'
 description 'BotRP custom Qbox character selection and creation experience'
-version '1.0.6'
+version '1.0.7'
 author 'BotRP'
 
-ui_page 'web/lobby-v106.html'
+ui_page 'web/lobby-v107.html'
 
 files {
-    'web/lobby-v106.html',
-    'web/botrp-character-v106.css',
+    'web/lobby-v107.html',
+    'web/botrp-character-v107.css',
     'web/app.js'
 }
 
