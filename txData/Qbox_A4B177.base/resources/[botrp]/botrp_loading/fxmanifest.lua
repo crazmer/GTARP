95fb2785887d77a41b9b8d88d3245ba018fb7faf
Atharva Mali <40512661+crazmer@users.noreply.github.com>
2026-09-14 14:53:22 +0530

    feat(botrp-loading): add reference-style FiveM loadscreen

--- txData/Qbox_A4B177.base/resources/[botrp]/botrp_loading/fxmanifest.lua
+++ txData/Qbox_A4B177.base/resources/[botrp]/botrp_loading/fxmanifest.lua
@@ -2,13 +2,20 @@ fx_version 'cerulean'
 game 'gta5'
 
 name 'botrp_loading'
-description 'BotRP player loading experience'
-version '0.1.1'
+description 'BotRP cinematic loading and player loading experience'
+version '0.2.0'
 author 'BotRP'
 
+-- Native FiveM loading screen shown while the client connects/loads.
+loadscreen 'web/load.html'
+loadscreen_manual_shutdown 'yes'
+loadscreen_cursor 'yes'
+
+-- In-game NUI used for transitional loading states after the native loadscreen.
 ui_page 'web/index.html'
 
 files {
+    'web/load.html',
     'web/index.html',
     'web/style.css',
     'web/app.js',
