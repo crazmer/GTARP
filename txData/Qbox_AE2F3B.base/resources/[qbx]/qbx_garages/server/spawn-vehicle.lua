aa3d57903772ca33cda7c7ae75365568a1ad2f64
Atharva Mali <40512661+crazmer@users.noreply.github.com>
2026-09-20 13:22:58 +0530

    fix(garage): return spawned vehicle netId after server dedupe

--- txData/Qbox_AE2F3B.base/resources/[qbx]/qbx_garages/server/spawn-vehicle.lua
+++ txData/Qbox_AE2F3B.base/resources/[qbx]/qbx_garages/server/spawn-vehicle.lua
@@ -144,7 +144,6 @@ lib.callback.register('qbx_garages:server:spawnVehicle', function (source, vehic
         Entity(veh).state:set('vehicleid', vehicleId, false)
         setVehicleStateToOut(vehicleId, veh, playerVehicle.modelName)
         TriggerEvent('qbx_garages:server:vehicleSpawned', veh)
-        return netId
         result = netId
     end, debug.traceback)
 
